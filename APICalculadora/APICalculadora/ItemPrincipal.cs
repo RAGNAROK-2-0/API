0037f0f1ecae83d1ab725bce722b693f46acbbd0
Victor-Morvy <morvy0@gmail.com>
2021-04-18 14:53:13 -0300

    conexão com banco de dados

---
+++ APICalculadora/APICalculadora/ItemPrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_calculadora.Models
+{
+    public class ItemPrincipal
+    {
+        public int idItem;
+        public string nomeItem { get; set; }
+        public DateTime dataInserido { get; set; }
+        public string nomeIcone { get; set; }
+
+        //private List<CustoVariado> custosVariados = new List<CustoVariado>();
+
+        //private List<CustoFixo> custosFixos = new List<CustoFixo>();
+
+        public ItemPrincipal(int id, string nome, DateTime data, string icone)
+        {
+            idItem = id;
+            nomeIcone = nome;
+            dataInserido = data;
+            nomeIcone = icone;
+        }
+    }
+}
