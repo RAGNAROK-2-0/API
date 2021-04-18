b9c3427b167e231534d0a1e012348b470339ef82
Victor-Morvy <morvy0@gmail.com>
2021-04-18 01:58:09 -0300

    First commit vinda da aula

---
+++ API_calculadora/API_calculadora/API_calculadora/CustoFixo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_calculadora.Models
+{
+    public class CustoFixo
+    {
+        public int idItem { get; set; }
+        public string descricao { get; set; }
+        public float valor { get; set; }
+        public string unidadeMedida { get; set; }
+        public float quantidade { get; set; }
+        public string data { get; set; }
+
+    }
+}
